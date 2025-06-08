57aa21a0ad056023a1ff81a709b626caeb8af9e5
oigo <michaelbichage2000@gmail.com>
2025-06-08 23:34:14 +0300

    done

---
+++ bodaFareCalculator/fare.js
@@ -0,0 +1,17 @@
+function calculateBodaFare(){
+let distanceInKm = prompt("Unafika wapi Mkubwa? Kilometer ngapi?:");
+    if(isNaN(distanceInKm) || distanceInKm <=0) {
+        alert("Tafadhali ingiza nambari sahihi ya kilomita.");
+    return;}
+    const baseFare = 50;
+    const chargePerKm = 15;
+    const tripFare = distanceInKm * chargePerKm;
+    const totalFare = baseFare + tripFare;
+
+    console.log(`Uko kwote? Io ni ${distanceInKm} km:`);
+    console.log(`Ukikalia Pikipiki: KES ${baseFare}`);
+    console.log(`Mpaka Uko: KES ${tripFare}`);
+    console.log(`Total: KES ${totalFare}`);
+    console.log("\n Panda Pikipiki!"); 
+}
+ calculateBodaFare();``
